00dd7877a4e5fb73b97866dfc427258043869b5b
mojila <ajilaksono@outlook.co.id>
2019-05-12 14:38:39 +0700

    create w

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
+const { Random } = require('random-js')
+const random = new Random()
 
 let data = fs.readFileSync(`${__dirname}/fruits.csv`).toString()
 let data_array = data.split('\n').map((d, i) => d = d.split(','))
 let data_array_headless = Array.from(data_array).slice(1, data_array.length)
 let data_array_number = Array(data_array_headless.length)
 
-// transform to bin
+// transform to number
 data_array_headless.map((d, i) => {
     let new_numbers = Array(Array.from(d).length)
 
@@ -35,10 +37,20 @@ data_array_headless.map((d, i) => {
     data_array_number[i] = new_numbers
 })
 
+let create_w = () => {
+    let result = random.integer(-1, 1) * random.realZeroToOneInclusive()
+
+    return result === 0 
+        ? create_w()
+        : result
+}
+
 // AND perceptron
 let treshold = 0
 let data_array_polute = data_array_number
-let w = [-0.3, 0.5, -0.4]
+let w = Array.from(Array(data_array_polute.length), (x, i) => create_w())
+
+console.log(w)
 
 let training_and = (treshold_temporary, data_temporary, w_temporary) => {
     Array.from(data_temporary).map((d, i) => {
@@ -53,9 +65,11 @@ let training_and = (treshold_temporary, data_temporary, w_temporary) => {
         total = total >= treshold_temporary ? 1 : 0
         
         // rekursif dengan w baru ketika total >= 1
-        if (total >= 1) training_and()
+        if (total >= 1) {
+
+        }
     })
 }
 
-training_and(treshold, data_array_polute, w)
+// training_and(treshold, data_array_polute, w)
 
